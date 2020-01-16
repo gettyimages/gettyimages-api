c19fbebf1c1e53a3e1d97a62e7b2857f8fffaa79
Mark Pitman <mark@pitman.io>
2020-01-16 10:57:15 -0800

    Add filters to python search example

--- code-samples/python/search-images-creative.py
+++ code-samples/python/search-images-creative.py
@@ -4,22 +4,24 @@ import json
 api_key = "YOUR_API_KEY"
 api_secret = "YOUR_API_SECRET"
 
-def search_for_creative_images(phrase, oauth):
-        """Search for creative images given a search phrase"""
-        url = "https://api.gettyimages.com/v3/search/images/creative?phrase=" + \
-            phrase
+# This script requires Python 3.6+ because it uses string interpolation
+
+def search_for_creative_images(phrase, orientations, number_of_people, oauth):
+        """Search for creative images given a search phrase and some filters"""
+        url = "https://api.gettyimages.com/v3/search/images/creative"
+        query_params = {"phrase": phrase, "orientations": orientations, "number_of_people": number_of_people}  
         headers = {
             "Api-Key": oauth["api_key"],
-            "Authorization": "Bearer " + oauth["access_token"]
+            "Authorization": f"Bearer {oauth['access_token']}"
         }
-        response = requests.request("GET", url, headers=headers)
+        response = requests.get(url, params=query_params, headers=headers)
         return response
 
 def get_client_credentials_token(key, secret):
     """Get an access token using client credentials grant"""
     url = "https://api.gettyimages.com/oauth2/token"
     payload = f"grant_type=client_credentials&client_id={key}&client_secret={secret}"
-    headers = {'content-type': 'application/x-www-form-urlencoded'}
+    headers = {"content-type": "application/x-www-form-urlencoded"}
     response = requests.request("POST", url, data=payload, headers=headers)
     oauth = json.loads(response.content)
     return oauth
@@ -28,5 +30,5 @@ def get_client_credentials_token(key, secret):
 
 oauth = get_client_credentials_token(api_key, api_secret)
 oauth["api_key"] = api_key
-search_response = search_for_creative_images("trees", oauth)
+search_response = search_for_creative_images("office workers", "square,vertical", 3, oauth)
 print(search_response.text)
