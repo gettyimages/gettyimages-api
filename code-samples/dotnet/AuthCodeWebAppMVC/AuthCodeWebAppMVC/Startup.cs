ab34cb545169e477c1e219067d335a16d552c09f
Chris Simmons <chris.simmons@gettyimages.com>
2020-12-16 13:52:13 -0800

    Further simplify Startup, remove unused file

--- code-samples/dotnet/AuthCodeWebAppMVC/AuthCodeWebAppMVC/Startup.cs
+++ code-samples/dotnet/AuthCodeWebAppMVC/AuthCodeWebAppMVC/Startup.cs
@@ -1,30 +1,18 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AuthCodeWebAppMVC
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //app.UseHttpsRedirection();
-
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
